c3ddc1f9c5af31679acc60b532696e0d36a677c8
alysssyo <alisya.yolanda22@gmail.com>
2025-09-30 23:58:27 +0700

    Add files via upload

---
+++ Pertemuan ke-2/tugas/Tugas Lab.Algoritma.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int A, B, Hasil;
+	
+	cout << "Nomor 1 - Penjumlahan" << endl;
+	cout << "Masukkan 2 angka" << endl;
+	
+	cout << "A =";
+	cin >> A;
+	
+	cout << "B =";
+	cin>> B;
+	
+	Hasil = A + B;
+	
+	cout << "A + B = " << Hasil << endl;
+	cout << "Hasil A + B = " << Hasil << endl;
+	
+	return 0;
+	
+}
